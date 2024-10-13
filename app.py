51467fdc2c2899cd18593e2f310e2b24de732bca
Anushka Raghuwanshi <anushka.raghuwanshi@sugaldamani.com>
2024-10-13 20:18:14 +0530

    changes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+from app.order_service.routes import order_bp
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Order Matching API"
+# Register the blueprint
+# app.register_blueprint(order_bp, url_prefix='/orders')
 
 if __name__ == "__main__":
     app.run(debug=True)
