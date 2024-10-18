8bfe2ad016425bb5e9f0375bdfe5d9a9fafe6f9b
Anushka Raghuwanshi <anushka.raghuwanshi@payworldindia.com>
2024-10-18 17:32:23 +0530

    changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
-from app.order_service.routes import order_bp
+#from app.order_service.routes import order_bp
 
 app = Flask(__name__)
 
 # Register the blueprint
-# app.register_blueprint(order_bp, url_prefix='/orders')
+#app.register_blueprint(order_bp, url_prefix='/orders')
 
 if __name__ == "__main__":
     app.run(debug=True)
